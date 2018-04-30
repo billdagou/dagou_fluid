58b1a5ac47d515ab2560e98fb851e9d34bf82350
Bill.Dagou <billdagou@gmail.com>
2018-04-30 22:41:18 +0800

    Refactor

---
+++ Classes/Traits/PageRenderer.php
@@ -0,0 +1,22 @@
+<?php
+namespace Dagou\DagouFluid\Traits;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+trait PageRenderer {
+    /**
+     * @var \TYPO3\CMS\Core\Page\PageRenderer
+     */
+    protected static $pageRenderer;
+
+    /**
+     * @return \TYPO3\CMS\Core\Page\PageRenderer
+     */
+    protected function getPageRenderer() {
+        if (static::$pageRenderer === NULL) {
+            static::$pageRenderer = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+        }
+
+        return static::$pageRenderer;
+    }
+}
