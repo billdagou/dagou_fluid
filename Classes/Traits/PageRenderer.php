ac6e57a23f14d60cf9d22263509ba3c3dd13e3e9
Bill.Dagou <billdagou@gmail.com>
2018-05-21 22:50:21 +0800

    Update PageRenderer Trait

--- Classes/Traits/PageRenderer.php
+++ Classes/Traits/PageRenderer.php
@@ -5,18 +5,9 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 trait PageRenderer {
     /**
-     * @var \TYPO3\CMS\Core\Page\PageRenderer
-     */
-    protected static $pageRenderer = NULL;
-
-    /**
      * @return \TYPO3\CMS\Core\Page\PageRenderer
      */
-    protected function getPageRenderer() {
-        if (static::$pageRenderer === NULL) {
-            static::$pageRenderer = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
-        }
-
-        return static::$pageRenderer;
+    protected static function getPageRenderer() {
+        return GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
     }
 }
