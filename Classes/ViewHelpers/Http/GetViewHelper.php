e3a4ab6719c82f09b79e2721ee3a9d519c59db85
Bill.Dagou <billdagou@gmail.com>
2018-08-29 21:03:23 +0800

    Add Http/GetViewHelper

--- Classes/ViewHelpers/Http/GetViewHelper.php
+++ Classes/ViewHelpers/Http/GetViewHelper.php
@@ -1,16 +1,18 @@
 <?php
 namespace Dagou\DagouFluid\ViewHelpers\Http;
 
-use TYPO3\CMS\Core\Utility\HttpUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
-class RedirectViewHelper extends AbstractViewHelper {
+class GetViewHelper extends AbstractViewHelper {
     public function initializeArguments() {
-        $this->registerArgument('url', 'string', 'The target URL to redirect to.');
-        $this->registerArgument('httpStatus', 'string', 'An optional HTTP status header.');
+        $this->registerArgument('param', 'string', 'The parameter name in URL.');
     }
 
+    /**
+     * @return mixed
+     */
     public function render() {
-        HttpUtility::redirect($this->arguments['url'] ?: $this->renderChildren(), $this->arguments['httpStatus']);
+        return GeneralUtility::_GET($this->arguments['param']);
     }
 }
