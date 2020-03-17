685bac3b77529b275505d4c3c2db633243d0815e
Bill.Dagou <billdagou@gmail.com>
2020-03-18 01:02:53 +0800

    Fix Seo:CanonicalViewHelper

--- Classes/ViewHelpers/Seo/CanonicalViewHelper.php
+++ Classes/ViewHelpers/Seo/CanonicalViewHelper.php
@@ -1,7 +1,6 @@
 <?php
 namespace Dagou\DagouFluid\ViewHelpers\Seo;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Seo\Canonical\CanonicalGenerator;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
@@ -10,8 +9,39 @@ class CanonicalViewHelper extends AbstractViewHelper {
      * @return string
      */
     public function render(): string {
-        if (preg_match('/href="([^"]*)/i', GeneralUtility::makeInstance(CanonicalGenerator::class)->generate(), $matches)) {
-            return $matches[1];
-        }
+        return $this->getCanonicalGenerator()
+            ->generate();
+    }
+
+    /**
+     * @return \TYPO3\CMS\Seo\Canonical\CanonicalGenerator
+     */
+    protected function getCanonicalGenerator(): CanonicalGenerator {
+        return new class() extends CanonicalGenerator {
+            /**
+             * @return string
+             */
+            public function generate(): string {
+                $href = '';
+
+                $this->signalSlotDispatcher->dispatch(CanonicalGenerator::class, 'beforeGeneratingCanonical', [&$href]);
+
+                if (empty($href) && (int)$this->typoScriptFrontendController->page['no_index'] === 1) {
+                    return '';
+                }
+
+                if (empty($href)) {
+                    $href = $this->checkContentFromPid();
+                }
+                if (empty($href)) {
+                    $href = $this->checkForCanonicalLink();
+                }
+                if (empty($href)) {
+                    $href = $this->checkDefaultCanonical();
+                }
+
+                return $href;
+            }
+        };
     }
 }
