451a2feb9f14f758755e223f639488ec6d56c4cb
Bill.Dagou <billdagou@gmail.com>
2021-04-29 17:06:48 +0800

    Add `transform` and `escape` fields in Link.TelViewHelper

--- Classes/ViewHelpers/Link/TelViewHelper.php
+++ Classes/ViewHelpers/Link/TelViewHelper.php
@@ -12,6 +12,8 @@ class TelViewHelper extends AbstractTagBasedViewHelper {
     public function initializeArguments() {
         parent::initializeArguments();
         $this->registerArgument('tel', 'string', 'The phone number to be turned into a link', TRUE);
+        $this->registerArgument('transform', 'array', 'Transform the phone number format in the link text');
+        $this->registerArgument('escape', 'bool', 'Escape special characters', FALSE, TRUE);
         $this->registerUniversalTagAttributes();
         $this->registerTagAttribute('name', 'string', 'Specifies the name of an anchor');
         $this->registerTagAttribute('rel', 'string', 'Specifies the relationship between the current document and the linked document');
@@ -24,7 +26,7 @@ class TelViewHelper extends AbstractTagBasedViewHelper {
      */
     public function render(): string {
         $linkHref = 'tel:'.$this->arguments['tel'];
-        $linkText = $this->arguments['tel'];
+        $linkText = $this->arguments['transform'] ? preg_replace($this->arguments['transform']['from'], $this->arguments['transform']['to'], $this->arguments['tel']) : $this->arguments['tel'];
 
         $tagContent = $this->renderChildren();
         if ($tagContent !== NULL) {
@@ -32,7 +34,7 @@ class TelViewHelper extends AbstractTagBasedViewHelper {
         }
 
         $this->tag->setContent($linkText);
-        $this->tag->addAttribute('href', $linkHref, TRUE);
+        $this->tag->addAttribute('href', $linkHref, $this->arguments['escape']);
         $this->tag->forceClosingTag(TRUE);
 
         return $this->tag->render();
