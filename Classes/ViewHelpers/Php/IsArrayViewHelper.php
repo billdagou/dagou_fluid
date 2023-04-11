a8a2827c318db6b5627d22c53afb172b90f898b4
Bill.大狗 <billdagou@gmail.com>
2023-04-11 08:21:10 +0800

    Add Php\IsArrayViewHelper

---
+++ Classes/ViewHelpers/Php/IsArrayViewHelper.php
@@ -0,0 +1,22 @@
+<?php
+namespace Dagou\DagouFluid\ViewHelpers\Php;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class IsArrayViewHelper extends AbstractViewHelper {
+    public function initializeArguments() {
+        $this->registerArgument('value', 'mixed', 'The variable being evaluated');
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param \TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext
+     *
+     * @return bool
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): bool {
+        return is_array($arguments['value'] ?? $renderChildrenClosure());
+    }
+}
