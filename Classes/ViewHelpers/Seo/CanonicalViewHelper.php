f4977cd985b0f1b7f1fc84323d2a56cda9bca953
Bill.大狗 <billdagou@gmail.com>
2024-05-07 15:39:08 +0800

    Fix Seo.CanonicalViewHelper

--- Classes/ViewHelpers/Seo/CanonicalViewHelper.php
+++ Classes/ViewHelpers/Seo/CanonicalViewHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace Dagou\DagouFluid\ViewHelpers\Seo;
 
+use TYPO3\CMS\Core\Domain\Page;
 use TYPO3\CMS\Seo\Canonical\CanonicalGenerator;
 use TYPO3\CMS\Seo\Event\ModifyUrlForCanonicalTagEvent;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
@@ -11,7 +12,9 @@ class CanonicalViewHelper extends AbstractViewHelper {
      */
     public function render(): string {
         return $this->getCanonicalGenerator()
-            ->generate();
+            ->generate([
+                'request' => $this->renderingContext->getRequest(),
+            ]);
     }
 
     /**
@@ -20,14 +23,17 @@ class CanonicalViewHelper extends AbstractViewHelper {
     protected function getCanonicalGenerator(): CanonicalGenerator {
         return new class() extends CanonicalGenerator {
             /**
+             * @param array $params
+             *
              * @return string
              */
-            public function generate(): string {
+            public function generate(array $params): string {
                 if ($this->typoScriptFrontendController->config['config']['disableCanonical'] ?? FALSE) {
                     return '';
                 }
 
-                $event = $this->eventDispatcher->dispatch(new ModifyUrlForCanonicalTagEvent(''));
+                $event = new ModifyUrlForCanonicalTagEvent('', $params['request'], new Page($this->typoScriptFrontendController->page));
+                $event = $this->eventDispatcher->dispatch($event);
                 $href = $event->getUrl();
 
                 if (empty($href) && (int)$this->typoScriptFrontendController->page['no_index'] === 1) {
